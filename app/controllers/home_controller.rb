8978f1ba76f42222004f6e6fa2c9cd79ab957f42
Sille Van Landschoot <info@sillevl.be>
2016-11-20 14:12:21 +0100

    show messages on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+      @message = Message.new
+      @messages = Message.all.reverse
   end
+
+  def message
+      @message = Message.create(message_params)
+      redirect_to root_path
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def message_params
+      params.require(:message).permit(:message)
+    end
 end
